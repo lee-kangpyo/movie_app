e771ed286f30a74bfdd46dda0518842343635cbd
MSI02 <darkzhon@hanmail.net>
2021-07-29 21:06:13 +0900

    props 시작

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+function Food(props){
+  console.log(props)
+  return <h1>I like potato</h1>
+}
+
 function App() {
   //return <div className="App" />;
-  return <div>Hello!!!</div> ;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Food fav="kimchi" something={true} papapapa={['hello', 1, 2, 3, 4, true]}/>
+    </div>
+  );
 }
 
 export default App;
