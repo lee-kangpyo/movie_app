cf2d7d256337d842a9868265c529efbb61c004db
MSI02 <darkzhon@hanmail.net>
2021-07-30 09:28:59 +0900

    1. 컴포넌트란 무었인가? 2. jsx는 자바스크립트와 html을 조합한 문법이다. 3. jsx를 이용한 컴포넌트(대문자로 시작) 작성 4. 데이터를 전달할때는 props를 사용 5. props는 객체로 변환되어 컴포넌트(함수)로 전달 6. ES6의 구조분해 할당을 사용

--- src/App.js
+++ src/App.js
@@ -1,21 +1,7 @@
 import React from 'react';
 
-function Food(props){
-<<<<<<< HEAD
-  return <h1>I like {props.fav}</h1>
-}
-
-function Food2(props){
-  var { fav } = props
+function Food({ fav }){
   return <h1>I like {fav}</h1>
-}
-
-function Food3({ fav }){
-  return <h1>I like {fav}</h1>
-=======
-  console.log(props)
-  return <h1>I like potato!sadfsaf</h1>
->>>>>>> 83c704e7f781f9494ac939c8ce01f33f31be4f26
 }
 
 function App() {
@@ -23,7 +9,10 @@ function App() {
   return (
     <div>
       <h1>Hello</h1>
-      <Food3 fav="kimchi"/>
+      <Food fav="kimchi"/>
+      <Food fav="ramen "/>
+      <Food fav="삼겹살"/>
+      <Food fav="chukumi"/>
     </div>
   );
 }
