b612a3d84e073fb3498cb61d6511efb5ba6f87a3
MSI02 <darkzhon@hanmail.net>
2021-08-03 10:06:18 +0900

    클래스형 컴포넌트로 state를 이용한 동적 변경

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+/*
 function Food({ name, picture, rating }){
   return (
     <div>
@@ -59,6 +59,35 @@ Food.propTypes = {
   name: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
   rating: PropTypes.number,
+}*/
+
+class App extends React.Component{
+  state = {
+    count:0
+  }
+
+  add = () => {
+    //this.setState({count:this.state.count+1}) // this.state.count는 성능 이슈가 생길수 있음
+    this.setState(current => ({
+      count: current.count+1
+    }))
+  }
+
+  minus = () => {
+    this.setState(current => ({
+      count: current.count-1
+    }))
+  }
+
+  render(){
+    return (
+        <div>
+          <h1>The number is: {this.state.count}</h1>
+          <button onClick={this.add}>Add</button>
+          <button onClick={this.minus}>Minus</button>
+        </div>
+    )
+  }
 }
 
 export default App;
