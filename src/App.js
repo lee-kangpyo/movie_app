3a6c524dda377fe5fa119b158a7a4d92e9e3215b
MSI02 <darkzhon@hanmail.net>
2021-08-05 09:46:25 +0900

    무비 모듈 추가 및 props로 데이터 전달하기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
 import React from 'react';
 
 import axios from 'axios';
+import Movie from './Movie';
 
 
 class App extends React.Component{
-
   state = {
     isLoading:true,
     movies:[],
   }
+
 
   componentDidMount(){
     // 영화 데이터 로딩
     this.getMovies();
   }
 
+
   getMovies = async () => {
-    const {data:{data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    const {data:{data:{movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
     this.setState({movies, isLoading:false}) // 키와 값의 변수 이름이 같으면 키값을 생략가능하다.
   }
 
 
   render(){
-    const { isLoading } = this.state;
-
-    return ( <div>{isLoading?"Loading...":"We are ready"}</div> )
+    const { isLoading, movies } = this.state;
+    console.log(movies)
+    return ( <div>{isLoading
+              ?"Loading..."
+              :movies.map((item)=>{
+                return<Movie id={item.id} year = {item.year} title={item.title} summary={item.summary} poster={item.medium_cover_image}/>;
+              })}</div> )
   }
 }
+
 
 export default App;
