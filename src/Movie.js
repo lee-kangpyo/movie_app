c781729174860f8fa53ad187a756e2f187ec34f4
MSI02 <darkzhon@hanmail.net>
2021-08-10 10:41:08 +0900

    css로 디자인하기

--- src/Movie.js
+++ src/Movie.js
@@ -7,15 +7,14 @@ function Movie({title, year, summary, poster, genres}){
     <div className="movie">
       <img src={poster} alt={title} title={title}/>
       <div className="movie__data">
-        <h3 className="moive__title" style={{ backgroundColor: 'red'}} >{title}</h3>
-        <h5 className="moive__year">{year}</h5>
+        <h3 className="movie__title" /*style={{ backgroundColor: 'red'}}*/ >{title}</h3>
+        <h5 className="movie__year">{year}</h5>
         <ul className="movie__genres">
           {genres.map( (genre, idx) => {
             return <li key = {idx} className="movie__genre">{genre}</li>;
           })}
         </ul>
-        <h5 className="movie__genres">{genres}</h5>
-        <p className="moive__summary">{summary}</p>
+        <p className="movie__summary">{summary}</p>
       </div>
     </div>
   )
