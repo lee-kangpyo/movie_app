641e78077bdf6e02948485a3f0570964bd464ccc
MSI02 <darkzhon@hanmail.net>
2021-08-17 17:45:25 +0900

    네비게이션 모듈 생성 및 app.js에 추가하 (a 태그는 페이지 리프레쉬를 하기 때문에 Link 태그를 사용한다.)

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import { HashRouter, Route} from 'react-router-dom';
 import About from './routes/About';
 import Home from './routes/Home';
 
+import Navigation from './components/Navigation';
+
 function App(){
   return (
     <HashRouter>
+      <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
     </HashRouter>
   );
 };
-
 
 export default App;
