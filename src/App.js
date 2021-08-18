9325ded3549176d8f2a8e70f466d407802148e70
MSI02 <darkzhon@hanmail.net>
2021-08-18 09:54:35 +0900

    상세보기 페이지 추가(route props 사용하기)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './css/App.css';
 import { HashRouter, Route} from 'react-router-dom';
+
 import About from './routes/About';
 import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 import Navigation from './components/Navigation';
 
@@ -12,6 +14,7 @@ function App(){
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 };
